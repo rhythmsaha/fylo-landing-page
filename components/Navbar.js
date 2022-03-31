5c52ffd272f2638b1f99a9229bf3a50d222d8c17
Rhythm Saha <rs.2001.saha@gmail.com>
2022-03-31 18:54:29 +0530

    navbar active state

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,8 +3,8 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <header className="flex py-6 px-6 items-center justify-between font-railway bg-primary-1">
-            <span className="relative h-8 w-20 flex justify-center items-center">
+        <header className="flex py-6 px-6 lg:py-12 lg:px-12 items-center justify-between font-railway bg-primary-1">
+            <span className="relative h-8 lg:h-16 w-20 lg:w-32 flex justify-center items-center">
                 <Image
                     src={"/images/logo.svg"}
                     alt="fylo_logo"
@@ -14,10 +14,10 @@ const Navbar = () => {
                 />
             </span>
 
-            <nav className="flex items-center space-x-4 text-sm tracking-wide">
-                <span>Features</span>
-                <span>Team</span>
-                <span>Sign In</span>
+            <nav className="flex items-center space-x-4 text-sm lg:text-lg lg:space-x-10 tracking-wide">
+                <span className="hover:underline cursor-pointer">Features</span>
+                <span className="hover:underline cursor-pointer">Team</span>
+                <span className="hover:underline cursor-pointer">Sign In</span>
             </nav>
         </header>
     );
